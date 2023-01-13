741a773085560b7849e52746b79639c3d2e732cb
vmrchs <vmrchs@gmail.com>
2023-01-13 16:10:51 -0300

    done for now.

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -5,10 +5,10 @@ export default function Interests() {
     <section className="interests">
       <h2 className="title">Interests</h2>
       <p>
-        Other than programming, I like to use my time to cook for my friends,
-        ride my bike, make art and go to museums. {"\n"} I'm very passionate
-        about music, and know how to play a few instruments very poorly. Other
-        than that I love being outdoors and doing physical activities.
+        Besides programming, I like to use my time to cook for my friends, ride
+        my bike, make art and go to museums. {"\n"} I'm very passionate about
+        music, and know how to play a few instruments very poorly. Other than
+        that, I love being outdoors and doing physical activities.
       </p>
     </section>
   );
