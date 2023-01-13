c215ccad36e6555eaf3bd4144a57b7ccb744949a
Vinnie Marchisio <vmrchs@Vinnies-MacBook-Air.local>
2023-01-13 16:06:46 -0300

    done for now.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ export default function About() {
       <h2 className="title">About</h2>
       <p>
         I'm a frontend developer currently learning React.{"\n"}I'm curious and
-        excited to learn new things, and aspire to become a fullstack developer
-        someday.
+        excited to learn new things, and aspiring to become a fullstack
+        developer someday.
       </p>
     </section>
   );
